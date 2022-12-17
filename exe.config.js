01554456022166dd6a8929e4f8f06e570670abb8
Neil Flatley <n.flatley@zengenti.com>
2022-12-17 01:52:44 +0000

    build: customised windows build to add icon and overwrite default node JS program properties

---
+++ exe.config.js
@@ -0,0 +1,19 @@
+const exe = require('@angablue/exe');
+const package = require('./packages/contensis-cli/package.json');
+
+const build = exe({
+  entry: '.',
+  out: './bin/contensis-cli.exe',
+  pkg: ['--public-packages', '"*"', '--public', '--compress', 'GZip'], // Specify extra pkg arguments
+  version: package.version,
+  target: 'latest-win-x64',
+  icon: './assets/icon.ico', // Application icons must be in .ico format
+  properties: {
+    FileDescription: 'Contensis CLI',
+    ProductName: 'Contensis CLI',
+    LegalCopyright: 'Zengenti Ltd',
+    OriginalFilename: 'contensis-cli.exe',
+  },
+});
+
+build.then(() => console.log('Windows x64 exe build completed'));
