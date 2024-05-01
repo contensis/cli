349d2eb39fbae86531544f92a9c68ea7af64598e
Neil Flatley <n.flatley@zengenti.com>
2024-05-01 19:14:10 +0100

    build: fix binary builds add polyfill to global with node script before the cli is launched

--- packages/contensis-cli/cli.js
+++ packages/contensis-cli/cli.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 // console.log(process.argv);
 
+/**
+ * Activate additional console output
+ */
 // process.env.debug = 'true';
 
+/**
+ * Polyfills to load before the CLI
+ */
+if (!globalThis.crypto) {
+  // Versions of Node < 20 do not have crypto attached to global by default
+  // and this is required by @contensis/html-canvas package
+  const crypto = require('node:crypto');
+  globalThis.crypto = crypto.webcrypto;
+}
+
+/**
+ * Run cli command or launch shell
+ */
 if (process.argv.length > 2)
   // run cli if args provided
   require('./dist');
